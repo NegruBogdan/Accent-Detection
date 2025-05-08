127f45485d81bd6d20cacef3bab9da8407ce5ede
Negru Liviu-Bogdan <73275449+NegruBogdan@users.noreply.github.com>
2025-05-09 01:35:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -110,26 +110,31 @@ def predict_accent(audio_path, model, processor, label_mapping, max_length=16000
 def main():
     st.title("Accent Detection from YouTube Video")
 
+    if 'cleanup_done' not in st.session_state:
+        st.session_state.cleanup_done = False
+
     url = st.text_input("Enter YouTube URL:")
 
-    if url:
+    if url and not st.session_state.cleanup_done:
         model, processor = load_model()
-
         label_mapping = load_label_mapping()
-        
+
         audio_path = extract_audio_from_video(url)
         video_path = "downloaded_video.mkv"
-        try:
-            if audio_path:
-                predicted_label, confidence = predict_accent(audio_path, model, processor, label_mapping)
-                if predicted_label is not None:
-                    st.write(f"Predicted Accent: {predicted_label}")
-                    st.write(f"Confidence: {confidence:.2f}")
-                else:
-                    st.write("Accent prediction failed.")
+
+        if audio_path:
+            predicted_label, confidence = predict_accent(audio_path, model, processor, label_mapping)
+            if predicted_label is not None:
+                st.write(f"Predicted Accent: {predicted_label}")
+                st.write(f"Confidence: {confidence:.2f}")
             else:
-                st.write("Failed to extract audio from the video.")
-        finally:
-            for file in [audio_path, video_path]:
-                if file and os.path.exists(file):
-                    os.remove(file)
+                st.write("Accent prediction failed.")
+        else:
+            st.write("Failed to extract audio from the video.")
+
+        for file in [audio_path, video_path]:
+            if file and os.path.exists(file):
+                os.remove(file)
+
+        st.session_state.cleanup_done = True
+
